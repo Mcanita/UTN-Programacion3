f5a974a0f7a9214ea3d969012fbedf22fffb01ab
MacanitaDB <105833395+Mcanita@users.noreply.github.com>
2024-09-05 00:37:27 -0300

    Explicación Clase String

---
+++ 2.POO/POO1/claseString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace claseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Maria";
+            nombre = "Hola como estas " + nombre;
+            Console.WriteLine(nombre);
+
+            nombre = Console.ReadLine();// SE LE PUEDE ASIGNAR NOMBRE DIRECTAMENTE 
+            Console.WriteLine(nombre);
+            nombre = "Hola como estas " + nombre;
+            Console.WriteLine(nombre);
+
+            int cantidad = nombre.Length; //ME DICE LA CANTIDAD DE CARACTERES 
+            nombre = nombre.ToUpper(); //CONVIERTE TODO A MAYUSCULA
+            nombre = nombre.ToLower(); //CONVIERTE TODO A MINUSCULA
+            nombre = nombre.Replace('a','e'); //PERMITE CAMBIAR UN CARACTER POR OTRO 
+            Console.ReadKey();
+        }
+    }
+}
