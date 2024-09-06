81d8420757ab9296329330f346a4e84721d4069c
MacanitaDB <105833395+Mcanita@users.noreply.github.com>
2024-09-06 00:09:34 -0300

    Controles y Eventos

---
+++ 4.WinForms/PrimeraSolucionWinForm/ejemploWinForm/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace ejemploWinForm
+{
+    partial class frmVentana1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.lblAceptar = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(230, 160);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(99, 38);
+            this.btnAceptar.TabIndex = 0;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // lblAceptar
+            // 
+            this.lblAceptar.AutoSize = true;
+            this.lblAceptar.Location = new System.Drawing.Point(238, 144);
+            this.lblAceptar.Name = "lblAceptar";
+            this.lblAceptar.Size = new System.Drawing.Size(81, 13);
+            this.lblAceptar.TabIndex = 1;
+            this.lblAceptar.Text = "Apriete el boton";
+            this.lblAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(175, 86);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(225, 86);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // frmVentana1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 293);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblAceptar);
+            this.Controls.Add(this.btnAceptar);
+            this.Name = "frmVentana1";
+            this.Text = "PrimerForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmVentana1_FormClosing);
+            this.Load += new System.EventHandler(this.frmVentana1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label lblAceptar;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+    }
+}
+
---
+++ 4.WinForms/PrimeraSolucionWinForm/ejemploWinForm/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejemploWinForm
+{
+    public partial class frmVentana1 : Form // PARTIAL ME PERMITE SEPARAR UNA CLASE EN VARIAS PARTES 
+    {
+        public frmVentana1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e) //evento por defecto
+        {
+            //MessageBox.Show("HOLA MUNDO");
+            string texto = txtNombre.Text;
+            lblAceptar.Text = "hola " + texto;
+        }
+
+        private void frmVentana1_Load(object sender, EventArgs e)// se ejecuta cuando carga el formulario
+        {
+            MessageBox.Show("Bienvenido...");
+        }
+
+        private void frmVentana1_FormClosing(object sender, FormClosingEventArgs e) //se ejecuta al cerrar
+        {
+            MessageBox.Show("Gracias por usar la app...");
+        }
+    }
+}
