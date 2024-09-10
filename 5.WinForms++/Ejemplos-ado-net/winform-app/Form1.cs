f1083ec9cf05ccd93388a4de066ee87585c0c652
MacanitaDB <105833395+Mcanita@users.noreply.github.com>
2024-09-09 23:54:16 -0300

    Primera lectura a DB

---
+++ 5.WinForms++/Ejemplos-ado-net/winform-app/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform_app
+{
+    public partial class frmPokemones : Form
+    {
+        public frmPokemones()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPokemones_Load(object sender, EventArgs e)
+        {
+            PokemonNegocio negocio = new PokemonNegocio();
+            dgvPokemons.DataSource = negocio.listar(); //lista los registros 
+
+        }
+    }
+}
